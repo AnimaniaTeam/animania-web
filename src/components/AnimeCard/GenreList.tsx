c3adba21ac7816ed874f237cf79cdfc2a321c03d
Maurício Bruno <mb.bruno.s@gmail.com>
2023-05-25 18:08:40 -0300

    feat: add anime card

---
+++ src/components/AnimeCard/GenreList.tsx
@@ -0,0 +1,21 @@
+import { Text } from "@chakra-ui/react";
+
+export function GenreList({ genres }: { genres: string[] }) {
+  return (
+    <>
+      {genres.map((genre) => (
+        <Text
+          key={genre}
+          color="#ffffff"
+          size="xs"
+          rounded="md"
+          bg="#3182CE"
+          px={2}
+        >
+          {genre}
+        </Text>
+      ))}
+    </>
+  );
+}
+
